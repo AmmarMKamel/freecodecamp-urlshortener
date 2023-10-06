2da22b05f126f760bbf723190b8524d6b1577cd4
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-10-07 02:23:24 +0300

    Install mongoose-sequence
    mongoose-sequence is used so that I can have an auto-incrementing id

--- urlSchema.js
+++ urlSchema.js
@@ -1,5 +1,8 @@
-// Import mongoose module
+// Import mongoose and mongoose-sequence module
 const mongoose = require("mongoose");
+const AutoIncrementFactory = require("mongoose-sequence");
+
+const AutoIncrement = AutoIncrementFactory(mongoose);
 
 // Assign mongoose.Schema to Schema variable
 const Schema = mongoose.Schema;
@@ -8,10 +11,10 @@ const Schema = mongoose.Schema;
 const urlSchema = new Schema({
 	// The schema requires a type and a "required" option for url
 	url: { type: String, required: true },
-
-	// The schema requires a type and a "required" option for shortUrl
-	shortUrl: { type: Number, required: true },
 });
+
+// Apply the AutoIncrement plugin to urlSchema and specify the field to auto increment
+urlSchema.plugin(AutoIncrement, { inc_field: "_id" });
 
 // Export default mongoose model of URL schema
 module.exports = mongoose.model("Url", urlSchema);
