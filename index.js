d0e4bba1ad611200213a26f2a729bc2b5077418a
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-10-07 02:06:10 +0300

    Install mongoose and connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+
+// Define the MongoDB connection string
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.oev1cbu.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+
+// Connect to MongoDB using Mongoose
+mongoose
+	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }) // Set connection options
+	.then(() => console.log("Connected to MongoDB Atlas")) // Log success message on successful connection
+	.catch((err) => console.error("Failed to connect to MongoDB Atlas:", err)); // Log error message on failed connection
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
